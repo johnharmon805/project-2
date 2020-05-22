1613f7829d20282b0be4e9179db27406eabf4337
Monica Nadal <missmonicanadal@gmail.com>
2020-05-21 19:28:46 -0700

    20200521 updated schema file with model for products table

--- Green Team Code/models/schema.sql
+++ Green Team Code/models/schema.sql
@@ -1,5 +1,18 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS store_db;
+CREATE DATABASE store_db;
+
+CREATE TABLE products_tb
+(
+	id int NOT NULL AUTO_INCREMENT,
+	product_name varchar(255) NOT NULL,
+    product_description varchar(255) NOT NULL,
+    product_category varchar(255) default 'miscellaneous',
+    essential BOOLEAN NOT NULL,
+    image_url varchar(500),
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT,
+	PRIMARY KEY (id)
+);
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
