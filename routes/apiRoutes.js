6e26026afa4327e4316e56de717c3b059a876140
Monica Nadal <missmonicanadal@gmail.com>
2020-05-26 20:19:41 -0700

    20200526 planting seeds

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,128 @@ module.exports = function (app) {
     })
   })
 
+  app.get('/bulk', function (req, res) {
+    db.Products.bulkCreate([
+      {
+        product_name: 'Bounty',
+        product_description: 'Paper Towel',
+        product_category: 'Household',
+        essential: true,
+        image_url: '',
+        price: 12.99,
+        stock_quantity: 100
+      },
+      {
+        product_name: 'Charmin',
+        product_description: 'Toilet Paper',
+        product_category: 'Household',
+        essential: true,
+        image_url: '',
+        price: 12.99,
+        stock_quantity: 100
+      },
+      {
+        product_name: 'Hand Sanitizer',
+        product_description: 'Hand Sanitizer',
+        product_category: 'Household',
+        essential: true,
+        image_url: '',
+        price: 7.99,
+        stock_quantity: 100
+      },
+      {
+        product_name: 'Face Mask',
+        product_description: 'Face Mask',
+        product_category: 'Household',
+        essential: true,
+        image_url: '',
+        price: 4.99,
+        stock_quantity: 100
+      },
+      {
+        product_name: 'Alexa',
+        product_description: 'Smart Speaker',
+        product_category: 'Electronics',
+        essential: false,
+        image_url: '',
+        price: 39.99,
+        stock_quantity: 20
+      },
+      {
+        product_name: 'Macbook Pro',
+        product_description: 'Computer Laptop',
+        product_category: 'Electronics',
+        essential: false,
+        image_url: '',
+        price: 1200.00,
+        stock_quantity: 10
+      },
+      {
+        product_name: 'Yoga Mat',
+        product_description: 'Yoga Mat',
+        product_category: 'Sports and Fitness',
+        essential: false,
+        image_url: '',
+        price: 27.99,
+        stock_quantity: 100
+      },
+      {
+        product_name: 'Baseball Bat',
+        product_description: 'Baseball Team Sports',
+        product_category: 'Sports and Fitness',
+        essential: false,
+        image_url: '',
+        price: 9.99,
+        stock_quantity: 100
+      },
+      {
+        product_name: 'Kleenex',
+        product_description: 'Tissue Paper',
+        product_category: 'Household',
+        essential: true,
+        image_url: '',
+        price: 4.99,
+        stock_quantity: 10
+      },
+      {
+        product_name: 'Bottled Water',
+        product_description: 'Plastic Water Bottle',
+        product_category: 'Pantry',
+        essential: true,
+        image_url: '',
+        price: 4.99,
+        stock_quantity: 100
+      }
+    ]).then(function (products) {
+      res.json(products)
+    })
+
+    db.Users.bulkCreate([
+      {
+        name: 'Andrew',
+        username: 'andrew',
+        password: 'password'
+      },
+      {
+        name: 'Monica',
+        username: 'monica',
+        password: 'password'
+      },
+      {
+        name: 'Eliska',
+        username: 'eliska',
+        password: 'password'
+      },
+      {
+        name: 'John',
+        username: 'john',
+        password: 'password'
+      }
+    ]).then(function (users) {
+      res.json(users)
+    })
+  })
+
   app.get('/api/users', function (req, res) {
     db.Users.findAll({}).then((users) => {
       res.json(users)
