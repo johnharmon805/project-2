e6a8c308ff48fb1fbcd1d08e401dc531e92d3dea
Andrew Lee <andrewimg@gmail.com>
2020-05-27 22:42:08 -0700

    got handlebars each loop to push the name of the products from the Products table to shop page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,25 @@ var db = require('../models')
 module.exports = function (app) {
   // Load index page
   app.get('/', function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.render('index', {
+    db.Products.findAll({}).then(function (dbExamples) {
+      res.render('home', {
         msg: 'Welcome!',
         examples: dbExamples
       })
     })
   })
 
+  // Cart Page
+  app.get('/cart', function (req, res) {
+    res.render('cart')
+  })
+
+  app.get('/shop', function (req, res) {
+    db.Products.findAll({}).then((data) =>{
+      res.render('shop', { Products: data })
+      console.log(data)
+    })
+  })
   // Load example page and pass in an example by id
   app.get('/example/:id', function (req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
