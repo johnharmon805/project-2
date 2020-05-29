1d38705fbc66bdbe728d7c131699510341cfb039
Eliska Patockova <patockovaeliska@gmail.com>
2020-05-28 21:19:10 -0700

    css and shop page edits

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,40 +1,45 @@
 var db = require('../models')
 
-module.exports = function (app) {
-  // Load index page
-  app.get('/', function (req, res) {
-    db.Products.findAll({}).then(function (dbExamples) {
-      res.render('home', {
-        msg: 'Welcome!',
-        examples: dbExamples
-      })
+module.exports = function(app) {
+    // Load index page
+    app.get('/', function(req, res) {
+        db.Products.findAll({}).then(function(dbExamples) {
+            res.render('home', {
+                msg: 'Welcome!',
+                examples: dbExamples
+            })
+        })
     })
-  })
 
-  // Cart Page
-  app.get('/cart', function (req, res) {
-    res.render('cart')
-  })
-
-  app.get('/')
-
-  app.get('/shop', function (req, res) {
-    db.Products.findAll({}).then((data) => {
-      res.render('shop', { Products: data })
-      console.log(data)
+    // Cart Page
+    app.get('/cart', function(req, res) {
+        res.render('cart')
     })
-  })
-  // Load example page and pass in an example by id
-  app.get('/example/:id', function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render('example', {
-        example: dbExample
-      })
-    })
-  })
 
-  // Render 404 page for any unmatched routes
-  app.get('*', function (req, res) {
-    res.render('404')
-  })
+    // Purchase Success Page
+    app.get('/success', function(req, res) {
+        res.render('Purchase-Success')
+    })
+
+    app.get('/')
+
+    app.get('/shop', function(req, res) {
+            db.Products.findAll({}).then((data) => {
+                res.render('shop', { Products: data })
+                console.log(data)
+            })
+        })
+        // Load example page and pass in an example by id
+    app.get('/example/:id', function(req, res) {
+        db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+            res.render('example', {
+                example: dbExample
+            })
+        })
+    })
+
+    // Render 404 page for any unmatched routes
+    app.get('*', function(req, res) {
+        res.render('404')
+    })
 }
