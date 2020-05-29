fb31847b0cb763ac7f16654cde5fd1c748ead77e
Andrew Lee <andrewimg@gmail.com>
2020-05-28 18:45:52 -0700

    added filepaths for image urls in sql table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
         product_description: 'Paper Towel',
         product_category: 'Household',
         essential: true,
-        image_url: '',
+        image_url: 'Paper-Towel.jpg',
         price: 12.99,
         stock_quantity: 100
       },
@@ -24,7 +24,7 @@ module.exports = function (app) {
         product_description: 'Toilet Paper',
         product_category: 'Household',
         essential: true,
-        image_url: '',
+        image_url: 'toilet-paper.png',
         price: 12.99,
         stock_quantity: 100
       },
@@ -33,7 +33,7 @@ module.exports = function (app) {
         product_description: 'Hand Sanitizer',
         product_category: 'Household',
         essential: true,
-        image_url: '',
+        image_url: 'hand-sanitizer.jpg',
         price: 7.99,
         stock_quantity: 100
       },
@@ -42,44 +42,8 @@ module.exports = function (app) {
         product_description: 'Face Mask',
         product_category: 'Household',
         essential: true,
-        image_url: '',
+        image_url: 'face-mask.jpg',
         price: 4.99,
-        stock_quantity: 100
-      },
-      {
-        product_name: 'Alexa',
-        product_description: 'Smart Speaker',
-        product_category: 'Electronics',
-        essential: false,
-        image_url: '',
-        price: 39.99,
-        stock_quantity: 20
-      },
-      {
-        product_name: 'Macbook Pro',
-        product_description: 'Computer Laptop',
-        product_category: 'Electronics',
-        essential: false,
-        image_url: '',
-        price: 1200.00,
-        stock_quantity: 10
-      },
-      {
-        product_name: 'Yoga Mat',
-        product_description: 'Yoga Mat',
-        product_category: 'Sports and Fitness',
-        essential: false,
-        image_url: '',
-        price: 27.99,
-        stock_quantity: 100
-      },
-      {
-        product_name: 'Baseball Bat',
-        product_description: 'Baseball Team Sports',
-        product_category: 'Sports and Fitness',
-        essential: false,
-        image_url: '',
-        price: 9.99,
         stock_quantity: 100
       },
       {
@@ -87,7 +51,7 @@ module.exports = function (app) {
         product_description: 'Tissue Paper',
         product_category: 'Household',
         essential: true,
-        image_url: '',
+        image_url: 'tissue.png',
         price: 4.99,
         stock_quantity: 10
       },
@@ -96,7 +60,7 @@ module.exports = function (app) {
         product_description: 'Plastic Water Bottle',
         product_category: 'Pantry',
         essential: true,
-        image_url: '',
+        image_url: 'bottled-water.jpg',
         price: 4.99,
         stock_quantity: 100
       }
