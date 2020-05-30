92d6cb1db390f4c77c5bfec9a921c91ea3136f03
Monica Nadal <missmonicanadal@gmail.com>
2020-05-29 20:58:07 -0700

    20200529 stripes like a tiger

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,45 +1,63 @@
 var db = require('../models')
 
 module.exports = function (app) {
-    // Load index page
-    app.get('/', function (req, res) {
-        db.Products.findAll({}).then(function (dbExamples) {
-            res.render('home', {
-                msg: 'Welcome!',
-                examples: dbExamples
-            })
+// Load index page
+  app.get('/', function (req, res) {
+    db.Products.findAll({}).then(function (dbExamples) {
+      res.render('home', {
+        msg: 'Welcome!',
+        examples: dbExamples
+      })
+    })
+  })
+
+  // Cart Page
+  app.get('/cart', function (req, res) {
+    res.render('cart')
+  })
+
+  // Purchase Success Page
+  app.get('/success', function (req, res) {
+    res.render('Purchase-Success')
+  })
+
+  app.get('/shop', function (req, res) {
+    db.Products.findAll({}).then((data) => {
+      res.render('shop', { Products: data })
+      console.log(data)
+    })
+  })
+  // Load example page and pass in an example by id
+  app.get('/example/:id', function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+      res.render('example', {
+        example: dbExample
+      })
+    })
+  })
+
+  app.get('/purchase', function (req, res) {
+    res.render('purchase')
+  })
+
+  app.post('/charge', (req, res) => {
+    try {
+      stripe.customers
+        .create({
+          name: req.body.name,
+          email: req.body.email,
+          source: req.body.stripeToken
         })
-    })
-
-    // Cart Page
-    app.get('/cart', function (req, res) {
-        res.render('cart')
-    })
-
-    // Purchase Success Page
-    app.get('/success', function (req, res) {
-        res.render('Purchase-Success')
-    })
-
-
-    app.get('/shop', function (req, res) {
-        db.Products.findAll({}).then((data) => {
-            res.render('shop', { Products: data })
-            console.log(data)
-        })
-    })
-    // Load example page and pass in an example by id
-    app.get('/example/:id', function (req, res) {
-        db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-            res.render('example', {
-                example: dbExample
-            })
-        })
-    })
-
-    // Render 404 page for any unmatched routes
-    app.get('*', function (req, res) {
-        res.render('404')
-    })
+        .then(customer =>
+          stripe.charges.create({
+            amount: req.body.amount * 100,
+            currencty: 'usd',
+            customer: customer.id
+          })
+        ).then(() => res.render('Purchase-Success'))
+        .catch(err => console.log(err))
+    } catch (err) {
+      res.send(err)
+    }
+  })
 }
-
