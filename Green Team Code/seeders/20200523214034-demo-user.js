3dd8c4393deee66b97a31c6f98553b4261708fd4
John Harmon <johnharmon805@gmail.com>
2020-05-29 21:51:57 -0700

    add to cart

--- Green Team Code/seeders/20200523214034-demo-user.js
+++ Green Team Code/seeders/20200523214034-demo-user.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -19,27 +19,25 @@ module.exports = {
       //   defaultValue: DataTypes.UUIDV1,
       //   primaryKey: true
       // },
-      product_name: "test_product_name",
-      product_description: "test_product_description",
-      product_category: "test_product_cat",
+      product_name: 'test_product_name',
+      product_description: 'test_product_description',
+      product_category: 'test_product_cat',
       essential: false,
-      image_url: "test_string_image",
+      image_url: 'test_string_image',
       price: 1099,
       stock_quantity: 45
       // createdAt: new Date(),
       // updatedAt: new Date()
-    }]);
-
-
+    }])
   },
 
   down: (queryInterface, Sequelize) => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
- 
+
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
   }
-};
+}
