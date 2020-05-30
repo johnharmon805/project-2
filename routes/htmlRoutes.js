7b7066c1ed5fd5740239c192bfce713e38fd97fc
Monica Nadal <missmonicanadal@gmail.com>
2020-05-29 20:17:22 -0700

    20200527 stripes html post route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -36,6 +36,29 @@ module.exports = function (app) {
         })
     })
 
+    // STRIPES STUFF
+    app.post('/charge', (req, res) => {
+        try {
+        stripe.customers
+            .create({
+                name: req.body.name,
+                email: req.body.email,
+                source: req.body.stripeToken
+            })
+            .then(customer =>
+                stripe.charges.create({
+                    amount: req.body.amount * 100,
+                    currency: 'usd',
+                    customer: customer.id
+                })
+            )
+            .then(() => res.render('Purchase - Success'))
+            .catch(err => console.log(err))
+    } catch (err) {
+        res.send(err);
+    }
+})
+
     // // Render 404 page for any unmatched routes
     // app.get('*', function (req, res) {
     //     res.render('404')
